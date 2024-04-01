c89cba96fef14327c4094627b6fcf18d26e5f9cd
Mmy2000 <yahianada005@gmail.com>
2024-04-01 02:58:06 +0200

    creating js code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
+var imgList = document.querySelectorAll('.item img')
+var lightboxContainer = document.querySelector('.ligthbox-container')
+var lightboxItem = document.querySelector('.ligthbox-item')
 
+for (let i = 0; i < imgList.length; i++) {
+   imgList[i].addEventListener('click' , function (e) {
+    lightboxContainer.classList.replace('d-none' , 'd-flex')
+    var imgSrc = e.target.getAttribute('src');
+    lightboxItem.style.backgroundImage = `url(${imgSrc})`
+   })
+    
+}
