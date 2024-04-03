4b947806ab228f75898e318b63aa3f470e053a38
Mmy2000 <yahianada005@gmail.com>
2024-04-03 22:11:56 +0200

    creating js code

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
 var imgList =Array.from( document.querySelectorAll('.item img'))
 var lightboxContainer = document.querySelector('.ligthbox-container')
 var lightboxItem = document.querySelector('.ligthbox-item')
+var nextBtn = document.getElementById('next')
+var prevBtn = document.getElementById('prev')
+var closeBtn = document.getElementById('close')
 var currentImageIndex;
+
+
 for (let i = 0; i < imgList.length; i++) {
    imgList[i].addEventListener('click' , function (e) {
     lightboxContainer.classList.replace('d-none' , 'd-flex')
-    var currentImageIndex = imgList.indexOf(e.target)
+    currentImageIndex = imgList.indexOf(e.target)
     var imgSrc = e.target.getAttribute('src');
     lightboxItem.style.backgroundImage = `url(${imgSrc})`
    })
-    
 }
+
+function nextSlide() {
+   currentImageIndex++;
+   var imgSrc = imgList[currentImageIndex].getAttribute('src')
+   lightboxItem.style.backgroundImage = `url(${imgSrc})`
+}
+nextBtn.addEventListener('click' , nextSlide)
